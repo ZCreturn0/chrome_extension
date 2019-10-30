c983fef4211d5259611f3ea97d1c95a53cd0f93a
ZCreturn0 <237350543@qq.com>
2019-10-30 23:22:26 +0800

    获取cookie

--- js/background.js
+++ js/background.js
@@ -1,12 +1,22 @@
+// 为 icon 加上 badge
 chrome.browserAction.setBadgeText({
     text: '憨'
 });
 chrome.browserAction.setBadgeBackgroundColor({
     color: [255, 0, 0, 255]
 });
+
+// 弹出 notification
 chrome.notifications.create(null, {
     type: 'basic',
     iconUrl: 'img/icon.png',
     title: 'hanser',
     message: 'hanser!hanser!hanser!hanser!hanser!'
+});
+
+// 获取 cookies
+chrome.cookies.getAll({
+    url: 'https://www.douyu.com/'
+},(cookies) => {
+    console.log(cookies);
 });
