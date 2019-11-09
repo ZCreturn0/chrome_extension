6b9646d642492a1fd00684b6f437e3c9f0b18283
ZCreturn0 <237350543@qq.com>
2019-11-09 18:43:21 +0800

    同步代码

--- js/background.js
+++ js/background.js
@@ -6,14 +6,6 @@ chrome.browserAction.setBadgeBackgroundColor({
     color: [255, 0, 0, 255]
 });
 
-// 弹出 notification
-chrome.notifications.create(null, {
-    type: 'basic',
-    iconUrl: 'img/icon.png',
-    title: 'hanser',
-    message: 'hanser!hanser!hanser!hanser!hanser!'
-});
-
 // 鱼吧 id
 const GROUP_ID = "5093593";
 let timestamp = Math.random();
@@ -35,28 +27,11 @@ function request(method, url, async, callback, header = {},data = null){
     xhr.send(data);
 }
 
-// 获取 cookies
-chrome.cookies.getAll({
-    // url: 'https://www.douyu.com/'
-    url: 'https://tieba.baidu.com/'
-},(cookies) => {
-    // console.log(cookies);
-    // let cookie = [];
-    // for(let item of cookies){
-    //     cookie.push(`${item.name}=${item.value}`);
-    // }
-    // 已经获取到了 cookie, 发送网络请求时会自动带上 cookie
-    request('get', `https://yuba.douyu.com/wbapi/web/group/head?group_id=${GROUP_ID}&timestamp=${timestamp}`, false, (result) => {
-        // 获取当前经验值,签到时必需参数
-        let exp = result.data.group_exp;
-        let sendData = new FormData();
-        sendData.append('group_id', GROUP_ID);
-        sendData.append('cur_exp', exp);
-        request('post', `https://yuba.douyu.com/ybapi/topic/sign?timestamp=${timestamp}`, false, (data) => {
-            console.log(data);
-        }, {
-            origin: 'https://yuba.douyu.com',
-            referer: `https://yuba.douyu.com/group/${GROUP_ID}`
-        },sendData);
-    });
-});
+$.ajax({
+    url: 'https://yuba.douyu.com/group/765880',
+    type: 'get',
+    success: function (res) {
+        console.log(res);
+        console.log(document.cookie);
+    }
+})
